48282ff10c68570eb463cb497334a1ac3e7e8b39
nichoth <nichoth@gmail.com>
2020-11-13 17:27:59 -0800

    nav kind of works

---
+++ src/script.js
@@ -0,0 +1,22 @@
+console.log('wooooooo')
+
+var isVisible = false
+document.getElementById('main-links').addEventListener('click', ev => {
+    update()
+})
+
+var btn = document.querySelector('button.clicker')
+btn.addEventListener('click', (ev) => {
+    console.log('click')
+    update()
+})
+
+function update () {
+    var linksEl = document.getElementById('main-links')
+    if (!isVisible) {
+        isVisible = true
+        return linksEl.classList.add("is-visible");
+    }
+    isVisible = false
+    linksEl.classList.remove("is-visible");
+}
