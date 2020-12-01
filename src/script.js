62df833f528d74d109bcee422efbb0fc15e56d7e
nichoth <nichoth@gmail.com>
2020-11-30 23:16:00 -0800

    more updates

--- src/script.js
+++ src/script.js
@@ -1,12 +1,17 @@
 var isVisible = false
-document.getElementById('main-links').addEventListener('click', ev => {
-    update()
-})
+var links = document.getElementById('main-links')
+if (links) {
+    links.addEventListener('click', ev => {
+        update()
+    })
+}
 
 var btn = document.querySelector('button.clicker')
-btn.addEventListener('click', (ev) => {
-    update()
-})
+if (btn) {
+    btn.addEventListener('click', (ev) => {
+        update()
+    })
+}
 
 function update () {
     var linksEl = document.getElementById('main-links')
